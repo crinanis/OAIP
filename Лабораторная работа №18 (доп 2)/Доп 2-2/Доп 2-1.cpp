1d6f80ad7a5ad2327fba32cb634e5dea15772e51
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:52:19 +0300

    Add files via upload

---
+++ Лабораторная работа №18 (доп 2)/Доп 2-2/Доп 2-1.cpp
@@ -0,0 +1,40 @@
+﻿//Вариант 11
+// Создать текстовый файл F1 не менее, чем из 6 строк, и записать в него информацию.Скопировать в файл F2 только те строки из F1, которые заканчиваются символом «а».
+
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+int main()
+{
+	using namespace std;
+	setlocale(LC_ALL, "Russian");
+	FILE* F1;
+	FILE* F2;
+	char str[255];
+
+	fopen_s(&F1, "f1.txt", "w");
+
+	for (int i = 0; i < 8; i++)
+	{
+		fflush(stdin);
+		printf("Строка  %d:\n", i + 1);
+		gets_s(str);
+		int r = strlen(str);
+		fputs(str, F1);
+		fputs("\n", F1);
+	}
+	fclose(F1);
+
+	fopen_s(&F1, "f1.txt", "r");
+	fopen_s(&F2, "f2.txt", "w");
+
+	while (fgets(str, 255, F1) != NULL)
+	{
+		if (str[strlen(str) - 2] == 'A' || str[strlen(str) - 2] == 'a')
+		{
+			fputs(str, F2);
+		}
+	}
+	fclose(F1);
+	fclose(F2);
+}
---
+++ Лабораторная работа №18 (доп 2)/Доп 2/Доп 2.cpp
@@ -0,0 +1,38 @@
+﻿//Вариант 11
+//Компоненты файла fileA –  целые числа, значения которых повторяются.  Получить файл fileB, образованный из fileA числами, которые встречаются в A ровно 2 раза.
+#include <stdio.h>
+#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "");
+	int* a;
+	int i, j;
+	FILE* f;
+	FILE* g;
+	fopen_s(&f, "a.txt", "r");
+	fopen_s(&g, "b.txt", "w");
+	
+	long fsize;
+	fseek(f, 0L, SEEK_END);
+	fsize = ftell(f);
+	fseek(f, 0L, SEEK_SET);
+
+	a = (int*)calloc(fsize, sizeof(int));
+	if (a == NULL) exit(1);
+	for (i = 0; i < fsize; i++) {
+		fscanf_s(f, "%d ", &a[i]);
+	}
+	for (i = 0; i < fsize; i++)
+	{
+		int	t = 0;
+		for (j = 0; j < fsize; j++) {
+			if (a[i] == a[j] && i != j) {
+				t = t + 1;
+			}
+		}
+		if (t == 1) fprintf_s(g, "%d ", a[i]);
+	}
+
+	fclose(f);
+	fclose(g);
+}
