8b7a3100e3d4f9a3bb384cb8bff781f74a477c5c
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:33:50 +0300

    Add files via upload

---
+++ Лабораторная работа 8 (доп 1)/Доп 1в 2/Доп 1в 2/Доп 1в 2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "russian");
+    double a = 1, b = 3, n = 200, h, x, s, i = 1, s1 = 0, s2 = 0, z;
+
+    h = (b - a) / (2 * n),
+        x = a + 2 * h;
+
+    do {
+        s2 = s2 + (pow(x, 3)-1);
+        x = x + h;
+        s1 = s1 + (pow(x, 3)-1);
+        x = x + h;
+        i = i + 1;
+    } while (i < n);
+
+    z = (h / 3) * ((pow(x, 3) - 1) + 4 * (pow((a + h), 3) - 1) + 4 * s1 + 2 * s2 + (pow(b, 3)-1));
+
+
+    cout << "z = " << z << endl;
+}
---
+++ Лабораторная работа 8 (доп 1)/Доп 1в 3/Доп 1в 3/Доп 1в 3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "russian");
+    double a = 2, b = 10, e = 0.0001, x, s, x1, z, func1, proizv1, pro, func2, proizv2, modul;
+
+    func1 = pow(a, 3) + a - 3;
+    proizv1 = 3 * pow(a, 2) + 1;
+    pro = func1 * proizv1;
+    cout << "pro= " << pro << endl;
+    if (func1 * proizv1 > 0) {
+        x1 = a;
+    }
+    else {
+        x1 = b;
+    }
+
+    cout << "Сначала x1= " << x1 << endl;
+
+    do {
+        x = x1;
+        func2 = pow(x, 3) + x - 3;
+        proizv2 = 3 * pow(x, 2) + 1;
+        x1 = x - (func2 / proizv2);
+        modul = fabs(x1 - x);
+    } while (modul > e);
+
+    cout << "В конце x1 = " << x1 << endl;
+}
