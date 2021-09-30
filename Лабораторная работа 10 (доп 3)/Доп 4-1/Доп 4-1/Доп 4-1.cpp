9cbe0cf46d903c645ebf98be3339c812d3a41747
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:41:12 +0300

    Add files via upload

---
+++ Лабораторная работа 10 (доп 3)/Доп 4-1/Доп 4-1/Доп 4-1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>			
+using namespace std;
+
+//1.	Используя битовые операции проверить, кратно ли четырём число А.
+int main (){
+    setlocale(LC_CTYPE, "russian");
+    int iA, iB, i, x = 0, F;
+    cout << "Введите число для проверки " << endl;
+    cin >> iA;
+    for (i = 0; i < 2; i++)
+    {
+        iB = iA & (1 << i);
+        if (iB == 0) x++;
+    }
+    if (x == 2) F = 1;
+    else F = 0;
+    cout << F;
+    
+}
