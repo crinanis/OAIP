682063df024f7ac9dac594c699d545bcea391fd3
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:45:33 +0300

    Add files via upload

---
+++ Лабораторная работа 16/Основа/Основа 1/Основа 1.cpp
@@ -0,0 +1,24 @@
+﻿// Написать функцию fsum с переменным числом параметров, которая находит сумму чисел типа int по формуле: S=a1*a2+a2*a3+a3*a4+ . . . .
+#include <iostream>
+using namespace std;
+
+
+int summa(int n, ...)
+{
+	int* p = &n;
+	int sum = 0;
+	int len = sizeof(p);
+	for (int i = 0; i <= len; i++)
+	{
+		sum += (*(p + i)) * (*(p + i + 1));
+	}
+	return sum;
+}
+void main()
+{
+	cout << summa(6, 4, 5, 1, 2, 3, 0) << endl;
+	cout << "-------------------------" << endl;
+	cout << summa(2, 3, 7, 4, 2, 4) << endl;
+	cout << "-------------------------" << endl;
+	cout << summa(1, 1, 1, 1, 1, 1);
+}
---
+++ Лабораторная работа 16/Основа/Основа/Основа.cpp
@@ -0,0 +1,60 @@
+﻿//1. Дана целочисленная прямоугольная матрица. Определить количество строк, содержащих хотя бы один нулевой элемент.
+#include <iostream>
+using namespace std;
+#define MINm  -10
+#define MAXm   10
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int n, n2, i, j, q = 0;
+	int o(int i, int n, int n2, int j, int** a);
+	int p(int i, int n, int n2, int** a, int q, int j);
+	cout << "Введите количество строк матрицы: "; cin >> n;
+	cout << "Введите количество столбцов матрицы: "; cin >> n2;
+	int** a = new int* [n];
+	for (i = 0; i < n; i++)
+		a[i] = new int[n2];
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n2; j++)
+		{
+			a[i][j] = (int)(((double)rand() /
+				(double)RAND_MAX) * (MAXm - MINm) + MINm);
+		}
+	}
+	o(i, n, n2, j, a);
+	p(i, n, n2, a, q, j);
+	
+}
+int o(int i, int n, int n2, int j, int** a)
+{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n2; j++)
+		{
+			cout.width(5);
+			cout << a[i][j];
+		}
+		cout << endl;
+	}
+	cout << "-----------------------------------------------------------------------------------" << endl;
+	return 0;
+}
+int p(int i, int n, int n2, int **a, int q, int j)
+{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n2; j++)
+		{
+			if (a[i][j] == 0)
+			{
+				q++;
+				break;
+			}
+		}
+	}
+	cout << "Количество строк с 0: " << q;
+	delete[] a;
+	return 0;
+}
