2f0cca5483f0e1f8038eb8b1dc11c682e9c5ad8c
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:49:04 +0300

    Add files via upload

---
+++ Лабораторная работа №17/ConsoleApplication1/Основа.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+double kor(double(*)(double), double, double);
+double kor1(double(*)(double), double, double);
+const double e = 1e-4;
+double k;
+
+double f(double x)
+{
+	return (pow(x, 3) + x - 3);
+}
+
+double s(double y)
+{
+	return (pow(cos(y), 3));
+}
+
+double kor(double(*f) (double), double a, double b)
+{
+	while (abs(a - b) > 2 * e)
+	{
+		double x;
+		x = (a + b) / 2;
+		if (f(a) * f(x) <= 0)
+			b = x;
+		else
+			a = x;
+	}
+	return (a + b) / 2;
+}
+
+double kor1(double(*s) (double), double c, double d)
+{
+	while (abs(c - d) > 2 * e)
+	{
+		double y;
+		y = (c + d) / 2;
+		if (s(c) * s(y) <= 0)
+			d = y;
+		else
+			c = y;
+	}
+	return (c + d) / 2;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double z, a, b, p, c, d;
+	cout << "Введите приближения а и b для первого уравнения:" << endl;
+	cin >> a >> b;
+	z = kor(f, a, b);
+	cout << " Корень первого уравнения равен = " << z << endl;
+	cout << "----------------------" << endl;
+	cout << "Введите приближения c и d для второго уравнения:" << endl;
+	cin >> c >> d;
+	p = kor1(s, c, d);
+	cout << " Корень второго уравнения равен = " << p;
+}
---
+++ Лабораторная работа №17/Основа 1/Основа 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int summa(int n, ...)
+{
+	int* a = &n;
+	int sum = 0;
+	int len = sizeof(a);
+	for (int i = 0; i <= len; i++)
+	{
+		sum += (*(a + i)) * (*(a + i + 1));
+	}
+	return sum;
+}
+void main()
+{
+	cout << summa(6, 4, 5, 1, 2, 3, 0) << endl;
+	cout << "-------------------------" << endl;
+	cout << summa(2, 3, 7, 4, 2, 4) << endl;
+	cout << "-------------------------" << endl;
+	cout << summa(1, 1, 1, 1, 1, 1);
+}
