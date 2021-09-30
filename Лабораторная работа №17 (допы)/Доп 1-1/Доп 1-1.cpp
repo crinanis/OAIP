024f30ac9a84522fd9e2f3c158e50178ed6d6578
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:49:23 +0300

    Add files via upload

---
+++ Лабораторная работа №17 (допы)/Доп 1-1/Доп 1-1.cpp
@@ -0,0 +1,25 @@
+﻿//Вариант 6
+#include <iostream>
+using namespace std;
+
+int sum(int n, ...)
+{
+	int* p = &n;
+	int s = 0;
+	p += 1;
+	for (int i = 1; i <= n / 2; i++) {
+		int* u = p + 1;
+		int* k = p + 2;
+		s += (*p) * (*u) - (*u) * (*k);
+		p = k;
+	}
+	return s;
+}
+void main()
+{
+	cout << sum(6, 1, 2, 3, 4, 5, 6, 7) << endl;
+	cout << "-------------------------" << endl;
+	cout << sum(9, 1, 1, 20, 5, 6, 7, 2, 5, 44, 2) << endl;
+	cout << "-------------------------" << endl;
+	cout << sum(15, 7, -2, 3, 3, 6, 7, 5, 8, 23, 16, -22, 65, 1, 6, 90, 1) << endl;
+}
---
+++ Лабораторная работа №17 (допы)/Доп 3-3/Доп 3-3.cpp
@@ -0,0 +1,23 @@
+﻿//Вариант 15
+#include <iostream>
+using namespace std;
+
+int tr(int n, ...)
+{
+	char tmp[33];
+	int* p = &n;
+	for (int i = 1; i <= n; i++, p++) {
+		_itoa_s(*p, tmp, 2);
+		cout << tmp << endl;
+	}
+	return 0;
+}
+int main()
+{
+	cout << tr(5, 4, 5, 1, 2) << endl;
+	cout << "---------------" << endl;
+	cout << tr(7, 4, 19, 3, 13, 5, 8) << endl;
+	cout << "---------------" << endl;
+	cout << tr(3, 54, 49) << endl;
+
+}
