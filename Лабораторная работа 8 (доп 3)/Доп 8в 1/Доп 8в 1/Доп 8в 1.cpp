4737fa83985ca6253e9f1b378f4d4231cbd826b7
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:34:04 +0300

    Add files via upload

---
+++ Лабораторная работа 8 (доп 3)/Доп 8в 1/Доп 8в 1/Доп 8в 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "russian");
+    double a = 0, b = 3, n = 200, h, x, s = 0, z;
+    h = (b - a) / n;
+    x = a;
+
+    do {
+        s = s + h * ((pow(x, 3) - 1) + pow((x + h), 3)) / 2;
+        x = x + h;
+    } while (x < (b - h));
+
+    z = s;
+    cout << "z = " << z << endl;
+}
---
+++ Лабораторная работа 8 (доп 3)/Доп 8в 4/Доп 8в 4/Доп 8в 4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_CTYPE, "russian");
+	double a = 1, b = 10, e = 0.0001, x;
+	do {
+		x = (a + b) / 2;
+		if ((pow(x, 3) + x - 2) * (pow(a, 3) + a - 2) <= 0) {
+			b = x;
+		}
+		else {
+			a = x;
+		}
+	} while (fabs(a - b) > (2 * e));
+	cout << "Значение х= " << x;
+}
