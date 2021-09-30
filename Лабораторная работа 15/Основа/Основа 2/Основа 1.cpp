c76059ca2fc9d200e408b9a625f9c57619b4e28f
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:43:42 +0300

    Add files via upload

---
+++ Лабораторная работа 15/Основа/Основа 2/Основа 1.cpp
@@ -0,0 +1,35 @@
+﻿//Вариант 2 (12-я лаба) Написать программу, реализующую выделение подстроки S1 длиной k с позиции номер n из строки. 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int k, n;
+	int* h(char* str, char* str2, int d, int n, int k);
+	char r, str[255], str2[225];
+	cout << "Введите строку: " << endl;
+	gets_s(str);
+	cout << "Введите длину: " << endl;
+	cin >> k;
+	cout << "Введите позицию: " << endl;
+	cin >> n;
+	int d = strlen(str);
+	*h(str, str2, d, n, k);
+}
+
+int* h(char* str, char* str2, int d, int n, int k)
+{
+	int i, j = 0, b = 0;
+	for (i = n; i < d - (d - (n + k)); i++)
+	{
+		str2[j] = str[i];
+		j++;
+	}
+	cout << str << endl;
+	for (i = 0; i < j; i++)
+	cout << str2[i];
+	cout << endl;
+	return 0;
+}
