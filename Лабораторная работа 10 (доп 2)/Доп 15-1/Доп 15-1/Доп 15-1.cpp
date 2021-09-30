c3d1c2e5e9f2b4163bbdb126ae7d6eb1a4df2d0a
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:41:08 +0300

    Add files via upload

---
+++ Лабораторная работа 10 (доп 2)/Доп 15-1/Доп 15-1/Доп 15-1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>			
+using namespace std;
+
+//1.	Используя битовые операции проверить, кратно ли двум число А.
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int a;
+	cout << "Введите число для проверки: ";
+	cin >> a;
+	if (a & 1)
+		cout << "Число нечетное";
+	else
+		cout << "Число четное";
+}
