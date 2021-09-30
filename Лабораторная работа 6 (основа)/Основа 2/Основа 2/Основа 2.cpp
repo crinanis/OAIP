95688fcced6f86c271dcb31a4e1405bd10ff3225
ksesha <72476438+crinanis@users.noreply.github.com>
2021-10-01 02:30:39 +0300

    Add files via upload

---
+++ Лабораторная работа 6 (основа)/Основа 2/Основа 2/Основа 2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "russian");
+    int sum = 0;
+    for (int k = 0; k < 200; k++) {
+        if (k % 3 == 0) {
+            sum = sum + k;
+            cout << "Числа, кратные 3: " << k << endl;
+        }
+    }
+    cout <<"Их сумма: " << sum << endl;
+}
+
---
+++ Лабораторная работа 6 (основа)/Основа/Основа/Основа.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_CTYPE, "Russian");
+	float b = -0.05, a = 1.72, i = -5, d, z;
+	while (i <= 5) {
+		i += 2;
+		if (i > 3 * b) {
+			d = a + b * i;
+		}
+		else {
+			d = tan(b) - a * i;
+		}
+		z = ((d * a) / 4) / ((3 * a * b) - (exp(1 + d) / 100));
+		cout << "Числа z: " << z << endl;
+		cout << "Числа d:" << d << endl;
+	}
+}
+
